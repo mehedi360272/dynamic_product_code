90c47c10fc00455c65205fb6ea924e920b6c5210
Mehedi <mehedi360272@gmail.com>
2024-11-28 16:56:26 +0600

    change QR_code code

--- controllers/qr_code_controller.py
+++ controllers/qr_code_controller.py
@@ -3,29 +3,41 @@ from odoo.http import request
 import qrcode
 from io import BytesIO
 
+
 class ProductQRCodeController(http.Controller):
 
     @http.route('/generate_qr_code/download/<int:product_id>', type='http', auth="user", csrf=False)
     def download_qr_code(self, product_id):
-        # Fetch the product
+        # Fetch the product based on product_id
         product = request.env['product.template'].browse(product_id)
+
         if not product.exists():
-            return request.not_found()
+            return request.not_found()  # If the product doesn't exist, return a 404
 
-        # Generate QR Code Data
+        # Prepare the QR code data: Product Name, Product Code, and UOM
         qr_data = f"Product Name: {product.name}\nProduct Code: {product.default_code}\nUnit of Measure: {product.uom_id.name}"
-        qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
-        qr.add_data(qr_data)
-        qr.make(fit=True)
 
-        # Create QR Code Image
-        qr_image = qr.make_image(fill_color="black", back_color="white")
+        # Generate the QR code
+        qr = qrcode.QRCode(
+            version=1,  # Version 1 means a small QR code
+            error_correction=qrcode.constants.ERROR_CORRECT_L,  # Error correction level
+            box_size=10,  # Size of each box in the QR code
+            border=4  # Border size
+        )
+
+        qr.add_data(qr_data)  # Add product data to the QR code
+        qr.make(fit=True)  # Adjust QR code size to fit the data
+
+        # Create the QR code image
+        qr_image = qr.make_image(fill='black', back_color='white')
+
+        # Save the image to a buffer in PNG format
         buffer = BytesIO()
-        qr_image.save(buffer, format="PNG")
-        buffer.seek(0)
+        qr_image.save(buffer, format='PNG')
+        buffer.seek(0)  # Reset the buffer's position to the beginning
 
-        # Set Filename
+        # Set the filename for the downloadable image
         filename = f"{product.default_code or 'product'}_qr_code.png"
 
-        # Return the QR Code as a downloadable file
+        # Return the QR code as a downloadable file
         return http.send_file(buffer, filename=filename, as_attachment=True)
